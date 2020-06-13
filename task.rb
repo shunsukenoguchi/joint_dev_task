8184c9d321fb5c32de9e125b0e1e05b9e64b7e00
Shunsuke Noguchi <bakutensanba89@yahoo.ne.jp>
2020-06-13 22:37:50 +0900

    課題17,18,20の修正

--- task.rb
+++ task.rb
@@ -149,7 +149,7 @@ end
 
 class UserQ17
   # 以下に回答を記載
-  def initialize(params)
+  def initialize(**params)
     @name = params[:name]
     @age = params[:age]
     @gender = params[:gender]
@@ -175,8 +175,7 @@ end
 
 class UserQ18
   # 以下に回答を記載
-
-  def initialize(params)
+  def initialize(**params)
     @name = params[:name]
     @age = params[:age]
   end
@@ -215,30 +214,31 @@ end
 class UserQ20
   # 以下に回答を記載
   attr_reader :name, :age
-  def initialize(name:, age:)
-    @name = name
-    @age = age
+  def initialize(**params)
+    @name = params[:name]
+    @age = params[:age]
   end
 end
 
 class Zoo
   # 以下に回答を記載
-  attr_reader :name, :entry_fee
-  def initialize(name:, entry_fee:)
-    @name = name
-    @entry_fee = entry_fee
+
+  def initialize(**params)
+    @name = params[:name]
+    @entry_fee = params[:entry_fee]
   end
   def info_entry_fee(user)
-    case user.age 
+    entry_fee = case user.age 
     when 0..5
-      puts "#{user.name}さんの入場料金は#{@entry_fee[:infant]}円です。"
+      @entry_fee[:infant]
     when 4..12
-      puts "#{user.name}さんの入場料金は#{@entry_fee[:children]}円です。"
+      @entry_fee[:children]
     when 13..64
-      puts "#{user.name}さんの入場料金は#{@entry_fee[:adult]}円です。"
+      @entry_fee[:adult]
     when 65..120
-      puts "#{user.name}さんの入場料金は#{@entry_fee[:senior]}円です。"
+      @entry_fee[:senior]
     end   
+    puts "#{user.name}さんの入場料金は#{entry_fee}円です。"
   end
 end
 
